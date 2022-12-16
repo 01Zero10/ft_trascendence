acf701f820fbb4703c1e17a95278d9165f9e36ed
BR7YAN <bremgonz@hotmail.it>
2022-12-16 17:47:25 +0100

    rimesso menu message, da sistemare css forse e collegare bene i pulsanti

--- front/src/chat_comp/ChannelMessage.tsx
+++ front/src/chat_comp/ChannelMessage.tsx
@@ -18,9 +18,43 @@ export default function ChannelMessage(props: MessageProps) {
 	let navigate = useNavigate();
 	const naturalTime = (new Date(props.createdAt)).toString().slice(0, 24);
 
-{/* <Menu position={"bottom-start"} closeDelay={400}>
+//  <Menu position={"bottom-start"} closeDelay={400}>
+// 				<Menu.Target>
+// 					<span id={props.id} className="message-user">{props.username}</span>
+// 				</Menu.Target>
+// 				{student.username !== props.username &&
+// 					<Menu.Dropdown>
+// 						<Menu.Item icon={<IconUser size={13} />} onClick={() => navigate(('/users/' + props.username))}>User profile</Menu.Item>
+// 						<Menu.Item icon={<IconMessage size={13} />}>Chat</Menu.Item>
+// 						<Menu.Item icon={<IconDeviceGamepad2 size={13} />}>Pong</Menu.Item>
+// 							TODO: inserire controllo admin
+// 						{(props.admin !== 0 && props.builder !== props.username) &&
+// 							<>
+// 								<Menu.Divider className={"divider"}></Menu.Divider>
+// 								<Menu.Label>Admin options</Menu.Label>
+// 								<Menu.Item icon={<IconBan size={13} />}>Ban</Menu.Item>
+// 								<Menu.Item icon={<IconMessageOff size={13} />}>Mute</Menu.Item>
+// 							</>}
+// 					</Menu.Dropdown>}
+
+// 			</Menu> 
+
+	return (
+		<>
+			{ student.username !== props.username ? <div id={props.id} className={props.class} style={{margin: "0 0 1% 0.5%"}}>
+					<div className="message_container">
+						<Avatar
+							className="avatarStyle"
+							src={props.avatar}
+							size={60}
+							radius={0}
+						/>
+						<div className="text_header_container">
+							<div className="usernameDetails_container">
+								{/* <span id={props.id} className="usernameHeader">{props.username}</span> */}
+								<Menu position={"bottom-start"} closeDelay={400}>
 				<Menu.Target>
-					<span id={props.id} className="message-user">{props.username}</span>
+					<span id={props.id} className="usernameHeader">{props.username}</span>
 				</Menu.Target>
 				{student.username !== props.username &&
 					<Menu.Dropdown>
@@ -37,21 +71,7 @@ export default function ChannelMessage(props: MessageProps) {
 							</>}
 					</Menu.Dropdown>}
 
-			</Menu> */}
-
-	return (
-		<>
-			{ student.username !== props.username ? <div id={props.id} className={props.class} style={{margin: "0 0 1% 0.5%"}}>
-					<div className="message_container">
-						<Avatar
-							className="avatarStyle"
-							src={props.avatar}
-							size={60}
-							radius={0}
-						/>
-						<div className="text_header_container">
-							<div className="usernameDetails_container">
-								<span id={props.id} className="usernameHeader">{props.username}</span>
+			</Menu> 
 								<span id={props.id} className="detailsHeader">{naturalTime}</span>
 							</div>
 							<div className="messageText">
