7410a71201a91d3883017aed144221f75027a58c
Michal Obrycki <mobrycki@MacBook-Air-di-Michal.local>
2022-12-19 18:11:07 +0100

    piccoli fix di stile ai button/text all'interno di un canale

--- front/src/chat_comp/ChannelBodyNav.tsx
+++ front/src/chat_comp/ChannelBodyNav.tsx
@@ -84,11 +84,11 @@ export default function ChannelBodyNav(props: any) {
 				</div>
 				<div className="channelOptionBar">
 					{/*TODO: inserire descrizione pulsanti*/}
-					{(props.room.name && props.room.builder.username === student.username)&& <div className={"divIconContainer"}><IconSettings onClick={() => props.setModalTypeOpen("options")}></IconSettings><p className={"parIconDescription"}>OPTION</p></div>}
-					{(props.room.name && props.room.builder.username === student.username)&& <div className={"divIconContainer"}><IconUserPlus onClick={() => props.setModalTypeOpen("add")}></IconUserPlus><p className={"parIconDescription"}>MEMBERS</p></div>}
-					{(props.room.name && props.admin) && <div className={"divIconContainer"}><IconGavel onClick={() => props.setModalTypeOpen("admin")}></IconGavel><p className={"parIconDescription"}>ADMIN</p></div>}
-					{(props.room.name && props.joined) && <div className={"divIconContainer"}><IconDoorExit onClick={() =>{ SetJoinFetch().then(); props.setRoom((prevState:any) => {return {...prevState, name:""}})}}></IconDoorExit><p className={"parIconDescription"}>LEAVE</p></div>}
-					{(props.room.name && !props.joined) && <div className={"divIconContainer"}><IconDoorEnter onClick={SetJoinFetch}></IconDoorEnter><p className={"parIconDescription"}>ENTER</p></div>}
+					{(props.room.name && props.room.builder.username === student.username)&& <div className={"divIconContainer"}><IconSettings size={20} onClick={() => props.setModalTypeOpen("options")}></IconSettings><p className={"parIconDescription"}>OPTIONS</p></div>}
+					{(props.room.name && props.room.builder.username === student.username)&& <div className={"divIconContainer"}><IconUserPlus size={20} onClick={() => props.setModalTypeOpen("add")}></IconUserPlus><p className={"parIconDescription"}>ADD USERS</p></div>}
+					{(props.room.name && props.admin) && <div className={"divIconContainer"}><IconGavel size={20} onClick={() => props.setModalTypeOpen("admin")}></IconGavel><p className={"parIconDescription"}>ADMIN</p></div>}
+					{(props.room.name && props.joined) && <div className={"divIconContainer"}><IconDoorExit size={20} onClick={() =>{ SetJoinFetch().then(); props.setRoom((prevState:any) => {return {...prevState, name:""}})}}></IconDoorExit><p className={"parIconDescription"}>LEAVE</p></div>}
+					{(props.room.name && !props.joined) && <div className={"divIconContainer"}><IconDoorEnter size={20} onClick={SetJoinFetch}></IconDoorEnter><p className={"parIconDescription"}>JOIN</p></div>}
 				</div>
 			</div>
 			{/* <svg style={{ position:"relative", height:"30%", width:"100%", rotate:"180deg", float:"right"}} ><image style={{ width:"20%"}} xlinkHref="/chat_decoration_top_mod_color2.svg"></image></svg> */}
