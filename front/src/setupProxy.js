44160f0613fafdcc3202148e762f0a92e75e1f61
Michal Obrycki <mobrycki@e3r12p1.42roma.it>
2023-01-09 17:06:33 +0100

    ..setupProxy.js in corso

---
+++ front/src/setupProxy.js
@@ -0,0 +1,18 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function (app) {
+  app.use(
+    '/login',
+    createProxyMiddleware({
+      target: `${process.env.REACT_APP_SERVER_URL}`,
+      changeOrigin: true,
+    }),
+  );
+  app.use(
+    '/graphql',
+    createProxyMiddleware({
+      target: `${process.env.REACT_APP_SERVER_URL}/graphql`,
+      changeOrigin: true,
+    }),
+  );
+};
